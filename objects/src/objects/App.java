5e739862690409515dfb62203e309bbc80ec26a8
Berberc <Berberc@172.29.252.213>
2015-11-16 21:57:14 +0100

    Project objects added

--- objects/src/objects/App.java
+++ objects/src/objects/App.java
@@ -3,7 +3,19 @@ package objects;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test dude");
+	
+		Student s1= new Student();
+		s1.matrikelNummer = 5430712;
+		s1.name ="Candan Berber";
+		s1.faecher = new String[] { "Philosophie"};
+		s1.gastHoerer = false;
+		s1.alter = 28;
+		System.out.println(s1.getMatrikelNummer() + " " + s1.getFaecher()[0] + " "  + s1.getName());
+		System.out.println(s1.getGastHoerer() + " " + s1.getAlter());
+		
+		
+		
+		
 
 	}
 
