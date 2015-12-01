d996457e479cc83b62ea43d725abca7913ae51e7
Berberc <Berberc@172.29.252.213>
2015-12-01 11:22:36 +0100

    project chars.java added

---
+++ chars/src/chars/AppChars.java
@@ -0,0 +1,15 @@
+package chars;
+
+public class AppChars {
+
+	public static void main(String[] args) {
+		Chars cc = new Chars();
+	
+	String toCheck = "praktikum";
+	
+	char[] charArray  = cc.toArray(toCheck);
+	cc.printArray(charArray);
+	}
+	
+	}
+
---
+++ chars/src/chars/Chars.java
@@ -0,0 +1,60 @@
+package chars;
+
+public class Chars {
+	void iterate(String s) {
+
+		for (int i = 0; i < s.length(); i++) {
+
+			char current = s.charAt(i);
+			System.out.println(current + "\t" + isLowerCase(current) + "\t" + decValue(current));
+
+		}
+
+	}
+
+	boolean isLowerCase(char current) {
+
+		if (current >= 'a' && current <= 'z') {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	int decValue(char current) {
+		int i = current;
+		return i;
+	}
+
+
+	char[] toArray(String s){
+		char[] charArray = new char[s.length()];
+		
+		for (int i = 0; i < s.length(); i++) {
+			
+			charArray[i] = s.charAt(i);
+			
+	
+			
+		}
+		return charArray;
+		
+	}
+	void printArray(char[] charArray){
+		
+		int current = 0;
+		int breakAt = charArray.length;
+		
+		while(current < breakAt){
+			
+			System.out.println("position " + current + " - value: " + charArray[current]);
+			current++;
+		}
+		
+	}
+}
+
+	
+
+
